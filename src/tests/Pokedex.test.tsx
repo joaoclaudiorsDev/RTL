993aa3be103e434b3aa8f9b073a3b3c1db977179
João Cláudio <joaoclaudiobio@gmail.com>
2023-06-28 13:31:46 -0300

    5.2

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -12,12 +12,18 @@ describe('5. Teste o component <Pokedex />', () => {
   test('exibe o próximo Pokémon da lista ao clicar no botão Próximo Pokémon', () => {
     renderWithRouter(<App />);
 
-    // botão
     const buttonNext = screen.getByTestId('next-pokemon');
     expect(buttonNext).toBeInTheDocument();
     expect(buttonNext).toHaveTextContent('Próximo Pokémon');
-    fireEvent.click(buttonNext);
-    fireEvent.click(buttonNext);
-    fireEvent.click(buttonNext);
+
+    const pokemonList = screen.getAllByTestId('pokemon-name');
+
+    for (let i = 0; i < pokemonList.length; i++) {
+      const currentPokemonElement = screen.getByTestId('pokemon-name');
+      expect(currentPokemonElement).toBeInTheDocument();
+      expect(currentPokemonElement?.textContent).toBe(pokemonList[i]?.textContent);
+
+      fireEvent.click(buttonNext);
+    }
   });
 });
