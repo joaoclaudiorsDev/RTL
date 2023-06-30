f5fe3aff1d5b37bbc46bcf79070138b977be836b
João Cláudio <joaoclaudiobio@gmail.com>
2023-06-30 17:27:51 -0300

    requisito 5 foi

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,17 +1,19 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
-import Pokedex from '../pages/Pokedex';
 import renderWithRouter from '../renderWithRouter';
 
 describe('5. Teste o component <Pokedex />', () => {
+  let renderedComponent;
+
+  beforeEach(() => {
+    renderedComponent = renderWithRouter(<App />);
+  });
+
   test('Exibe um h2 com texto "Encountered Pokémon"', () => {
-    renderWithRouter(<App />);
     expect(screen.getByText('Encountered Pokémon')).toBeInTheDocument();
   });
 
   test('exibe o próximo Pokémon da lista ao clicar no botão Próximo Pokémon', () => {
-    renderWithRouter(<App />);
-
     const buttonNext = screen.getByTestId('next-pokemon');
     expect(buttonNext).toBeInTheDocument();
     expect(buttonNext).toHaveTextContent('Próximo Pokémon');
@@ -25,5 +27,31 @@ describe('5. Teste o component <Pokedex />', () => {
 
       fireEvent.click(buttonNext);
     }
+  });
+
+  test('Deve existir um botão de filtragem para cada tipo de Pokémon', () => {
+    const pokemonTypes = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+
+    expect(typeButtons).toHaveLength(pokemonTypes.length);
+
+    typeButtons.forEach((button, index) => {
+      expect(button).toHaveTextContent(pokemonTypes[index]);
+    });
+  });
+
+  test('O texto do botão deve corresponder ao nome do tipo, ex.: Psychic', () => {
+    const psychicTypeButton = screen.getByText('Psychic');
+    expect(psychicTypeButton).toBeInTheDocument();
+  });
+
+  test('O botão All precisa estar sempre visível.', () => {
+    const allButton = screen.getByText('All');
+    expect(allButton).toBeInTheDocument();
+  });
+
+  test('Ao carregar a página, o filtro selecionado deve ser All', () => {
+    const allButton = screen.getByText('All');
+    expect(allButton).toHaveClass('filter-button');
   });
 });
